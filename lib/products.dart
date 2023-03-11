5f27fcd531564d7d6b06c893c31b6022b24af4e5
miydryg <nazar.kryzhanivsky@gmail.com>
2023-03-11 12:13:52 +0200

    Container, Column, Row, Expanded, crossAxisAlignment, mainAxisAlignment

---
+++ lib/products.dart
@@ -0,0 +1,12 @@
+class Product {
+  String name;
+  String description;
+  double price;
+  String imageUrl;
+
+  Product({
+    this.name = 'Product',
+    this.description = 'description',
+    this.price = 1000,
+    this.imageUrl = 'https://picsum.photos/200'});
+}
